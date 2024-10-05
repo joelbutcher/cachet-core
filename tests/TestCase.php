dc8d0f83c6e24d4575cb12539f820e52199e1fc3
Joel Butcher <joelbutcher@users.noreply.github.com>
2024-10-05 18:31:05 +0100

    Don't run seeders for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,9 @@ namespace Cachet\Tests;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\Contracts\Config;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Orchestra\Testbench\Workbench\Workbench;
 
 abstract class TestCase extends Orchestra
 {
@@ -30,5 +32,14 @@ abstract class TestCase extends Orchestra
             'database.default' => 'testing',
             // 'query-builder.request_data_source' => 'body',
         ]);
+    }
+
+    public static function cachedConfigurationForWorkbench(): Config
+    {
+        $config = Workbench::configuration();
+
+        $config['seeders'] = false;
+
+        return $config;
     }
 }
