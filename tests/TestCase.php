f107a2e3262b5819641405a193e3bde03b9f1306
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-06 21:48:17 +0800

    Fixes seeding issue on Testbench 9
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,9 +5,7 @@ namespace Cachet\Tests;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
-use Orchestra\Testbench\Contracts\Config;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Orchestra\Testbench\Workbench\Workbench;
 
 abstract class TestCase extends Orchestra
 {
@@ -32,14 +30,5 @@ abstract class TestCase extends Orchestra
             'database.default' => 'testing',
             // 'query-builder.request_data_source' => 'body',
         ]);
-    }
-
-    public static function cachedConfigurationForWorkbench(): Config
-    {
-        $config = Workbench::configuration();
-
-        $config['seeders'] = false;
-
-        return $config;
     }
 }
